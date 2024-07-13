0570578a6bed23f6b4e480468caacc966d81bf77
szolton <szolton.designs@gmail.com>
2024-07-12 20:41:19 -0400

    added to the workday schedule page

--- src/components/WebScheduler.js
+++ src/components/WebScheduler.js
@@ -16,11 +16,11 @@ function WebPlanner() {
             <section className="container mx-auto">
                 <div className="title-headers text-center mt-10">
                     <h2 className="mt-2 pt-5 text-center" style={{ fontFamily: 'Bebas Neue', color: '#6ac9cb', fontSize: '48px' }}>
-                        A Workday Scheduler
+                        A Workday Calendar
                     </h2>
                     </div>
                     <p className="mb-3 text-center text-gray-600 px-10">
-    The purpose of this project was to work in a group setting and apply everything learned in class over the past six weeks to create a real-world front-end application for portfolios. We integrated interactive daily tasks with weather and news updates using APIs. The project features a cool-toned color palette, utilizes Bulma styling, and updates the date daily.
+                    The purpose of this project was to work in a group setting and apply everything learned in class over the past six weeks to create a real-world front-end application for portfolios. We integrated interactive daily tasks with weather and news updates using APIs. The project features a cool-toned color palette, utilizes Bulma styling, and updates the date daily.
     To see more of my project, please follow this link to my GitHub: 
     <a href="https://szolton.github.io/05-challenge-week5/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-900 hover:underline ml-2 text-gray-600">Daily Interactive Planner</a>, or look at my code:<a href="https://github.com/szolton/05-challenge-week5" target="_blank" rel="noopener noreferrer" className="hover:text-gray-900 hover:underline ml-2 text-gray-600">here</a>.
 </p>
@@ -47,17 +47,18 @@ function WebPlanner() {
                         <img src={colorPaletteImage} className="object-contain rounded shadow-md h-full mx-auto" alt="Color Palette" />
                     </div>
 
-                    <h2 className="text-center text-6xl text-teal-500 font-bold my-8" style={{ fontFamily: 'Bebas Neue', color: '#6ac9cb', fontSize: '48px' }}>
+                    <h2 className="text-center text-6xl text-teal-500 my-8" style={{ fontFamily: 'Bebas Neue', color: '#6ac9cb', fontSize: '48px' }}>
                         In Conclusion
                     </h2>
                     <div className="flex justify-center">
                         <ul className="list-disc list-inside text-gray-600 text-left">
-                            <li>You can scroll down, and see time blocks for that day during the business hours of 9-5.</li>
+                            <li>The user can open the planner, and see the current date and time that's displayed at the top of the page.</li>
+                            <li>Yyou can scroll down, and see time blocks for that day during the business hours of 9-5.</li>
                             <li>The time blocks are color-coded for past, present, and future events.</li>
                             <li>When you click on a time block, you can add text that saves straight to the page, as well as the save button.</li>
-                            <li>The cool feature I added is editable text and being able to keep it on the page without having to press save</li>
-                            <li>Text is then saved in local storage, where when you refresh the page, it shows up</li>
-                            <li>You can also edit it and remove text as well</li>
+                            <li>The cool feature I added is editable text and being able to keep it on the page without having to press save.</li>
+                            <li>Text is then saved in local storage, where when you refresh the page, it shows up.</li>
+                            <li>You can also edit it and remove text as well.</li>
                         </ul>
                     </div>
                 </div>
