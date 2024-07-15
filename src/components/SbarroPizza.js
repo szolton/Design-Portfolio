9e5125fc7a78e626e3308e031676d2fb7fff0cf5
szolton <szolton.designs@gmail.com>
2024-07-15 14:11:30 -0400

    updated the sbarro and mag pages for responsiveness

--- src/components/SbarroPizza.js
+++ src/components/SbarroPizza.js
@@ -29,7 +29,7 @@ const SbarroPizza = () => {
           Together, I worked with Sbarro to create a new brand identity that captured their Italian roots with a modern, refreshed look that's fun and engaging. Using Adobe InDesign, Illustrator, Photoshop and AfterEffects, we created a brand identity with a clean, organic logo that's reflective of "A taste of Italy on the go." In this brand identity exploration, you will find the new logo for the company, some process sketches, a brand guide, a mobile app design, pizza box packaging, and social media campaign ads.
         </p>
 
-        <div className="columns-1 p-3 md:p-10 m-5 md:m-10">
+        <div className="columns-1 p-1 md:p-7 m-1 md:m-10">
           <img src={logoPortfolio} className="w-300 h-200 object-cover rounded shadow-md" alt="final logo design" />
         </div>
 
@@ -49,7 +49,7 @@ const SbarroPizza = () => {
           Original Sbarro logo designs I referenced, and the beginning stages of my design process where you can see sketches and other iterations of the logo I explored as design solutions.
         </p>
 
-        <div className="columns-1 p-3 md:p-10 m-5 md:m-10">
+        <div className="columns-1 p-1 md:p-10 m-1 md:m-10">
           <img src={backgrounds} className="w-300 h-200 object-cover rounded shadow-md" alt="logo on diff backgrounds" />
           <p className="mb-3 text-center text-gray-600 mt-2">
             Here, I explored how the logo would look against different backgrounds.
@@ -57,37 +57,36 @@ const SbarroPizza = () => {
         </div>
 
         <div className="max-w-full mx-auto h-auto overflow-hidden rounded shadow-md">
-  <div className="aspect-w-16 aspect-h-9">
-    <iframe 
-      title="vimeo-player" 
-      src="https://player.vimeo.com/video/745628232?h=0d3d1ad992" 
-      width="1100" 
-      height="625" 
-      frameBorder="0" 
-      allowFullScreen
-    ></iframe>
-  </div>
-  <p className="text-center text-gray-600 mt-3 mb-10">
-    A quick video animation of how the logo might look at the end of an advertisement.
-  </p>
-</div>
+          <div className="aspect-w-16 aspect-h-9">
+            <iframe 
+              title="vimeo-player" 
+              src="https://player.vimeo.com/video/745628232?h=0d3d1ad992" 
+              width="1100" 
+              height="625" 
+              frameBorder="0" 
+              allowFullScreen
+            ></iframe>
+          </div>
+          <p className="text-center text-gray-600 mt-3 mb-10">
+            A quick video animation of how the logo might look at the end of an advertisement.
+          </p>
+        </div>
 
-
-        <div className="columns-1 p-3 md:p-10 m-5 md:m-10">
+        <div className="columns-1 p-1 md:p-10 m-1 md:m-10">
           <img src={pizzaBoxMockup} className="w-300 h-200 object-cover rounded shadow-md" alt="pizza box mockup" />
           <p className="mb-3 text-center text-gray-600 mt-2">
             Above, I explored how the logo might look printed on the pizza box packaging.
           </p>
         </div>
 
-        <div className="columns-1 p-3 md:p-10 m-5 md:m-10">
+        <div className="columns-1 p-1 md:p-10 m-1 md:m-10">
           <img src={socialMediaMockups} className="w-300 h-200 object-cover rounded shadow-md" alt="social media ad mockup" />
           <p className="mb-3 text-center text-gray-600 mt-2">
             Here are some mockups of how the new brand identity might look on social media advertisements.
           </p>
         </div>
 
-        <div className="columns-1 p-3 md:p-10 m-5 md:m-10">
+        <div className="columns-1 p-1 md:p-10 m-1 md:m-10">
           <img src={appMockup} className="w-300 h-200 object-cover rounded shadow-md" alt="mobile app mockup" />
           <p className="mb-3 text-center text-gray-600 mt-2">
             Above is a mobile app design to make ordering on the go quick and easy.
--- src/components/magazine.js
+++ src/components/magazine.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../assets/style.css';
 import Footer from './Footer'; // Importing the Footer component
 
-
 import magazineMockup1 from '../assets/images/MAGAZINES/cmba magazine mockups 1.png';
 import magazineMockup2 from '../assets/images/MAGAZINES/cmba magazine mockups 2.png';
 import magazineMockup3 from '../assets/images/MAGAZINES/cmba magazine mockups 3.png';
@@ -13,10 +12,10 @@ import legalDirectory2 from '../assets/images/MAGAZINES/legal directory 2.png';
 const Magazine = () => {
   return (
     <div>
-      <h4 className="text-left p-5 pl-7" style={{ fontFamily: 'FranklinGothic URW', color: '#6ac9cb', fontSize: '18px' }}>
+      <h4 className="text-left p-5 pl-7 mobile-padding" style={{ fontFamily: 'FranklinGothic URW', color: '#6ac9cb', fontSize: '18px' }}>
         Graphic Designer, Illustrator, and Web Designer based in Cleveland, OH.
       </h4>
-      <div className="columns-1 p-10 m-10">
+      <div className="columns-1 p-10 m-10 mobile-padding">
         <h1 className="-mt-10" style={{ fontFamily: 'Bebas Neue', color: '#6ac9cb', fontSize: '48px', textAlign: 'center' }}>
           Magazine Spreads
         </h1>
@@ -24,24 +23,24 @@ const Magazine = () => {
           For the Cleveland Metropolitan Bar Association, I created several designs that included badges, a monthly Bar Journal magazine, advertisements and flyers, and the legal directory for committees within the association. I carefully followed their brand guidelines and enjoyed adding touches of creativity to the work.
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="w-full md:w-auto px-1 md:px-0">
+          <div className="w-full md:w-auto px-1 md:px-0 mobile-padding">
             <img src={magazineMockup1} className="w-full h-auto object-cover rounded shadow-md" alt="brochure design mockup" />
             <p className="mb-3 text-center text-gray-600 mt-2">Layout Designs for the CMBA's monthly Bar Journal Magazine.</p>
           </div>
-          <div className="w-full md:w-auto px-1 md:px-0">
+          <div className="w-full md:w-auto px-1 md:px-0 mobile-padding">
             <img src={magazineMockup2} className="w-full h-auto object-cover rounded shadow-md" alt="cmba magazine spread mockups" />
           </div>
-          <div className="w-full md:w-auto px-1 md:px-0">
+          <div className="w-full md:w-auto px-1 md:px-0 mobile-padding">
             <img src={magazineMockup3} className="w-full h-auto object-cover rounded shadow-md" alt="cmba magazine spread mockups" />
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4 mobile-padding">
           <div className="col-span-2">
             <img src={flyerAd} className="w-full h-300 object-cover rounded shadow-md" alt="cmba flier design" />
             <p className="mb-3 text-center text-gray-600 mt-2">Fliers I designed for the association to assist the marketing dept.</p>
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4 mobile-padding">
           <img src={legalDirectory1} className="w-full h-100 object-cover rounded shadow-md" alt="cmba magazine legal directory mockup" />
           <img src={legalDirectory2} className="w-full h-100 object-cover rounded shadow-md" alt="cmba magazine legal directory mockup" />
         </div>
