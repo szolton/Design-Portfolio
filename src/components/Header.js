21c8139ba8b3aa2e6f4a0948d227ae8b9f38c369
SLZolton <slzolton@SLZolton-27.local>
2024-07-19 09:54:23 -0400

    updated the header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,13 +23,13 @@ const Header = () => {
   };
 
   return (
-    <header className="nav-header bg-custom-turquoise text-white py-6">
+    <header className="nav-header bg-custom-turquoise text-white py-6 custom-header">
       <div className="w-full flex flex-col md:flex-row justify-between items-center">
-        <Link to="/" className="order-2 md:order-1 mx-auto md:mx-0 md:mr-auto">
-          <h1 className="text-center md:text-left" style={{ fontFamily: 'Bebas Neue', fontSize: '90px' }}>Stephanie Zolton</h1>
+        <Link to="/" className="order-1 md:order-2 mx-auto md:mx-0 md:mr-auto mb-2 md:mb-0">
+          <h1 className="text-center md:text-left text-7xl md:text-8xl" style={{ fontFamily: 'Bebas Neue' }}>Stephanie Zolton</h1>
         </Link>
-        <nav className="w-full md:w-auto order-1 md:order-2 mt-4 md:mt-0">
-          <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 items-center md:justify-end w-full">
+        <nav className="w-full md:w-auto order-2 md:order-2">
+          <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-8 items-center md:justify-end w-full text-md md:text-base">
             <li className="flex-1 md:flex-none">
               <Link to="/Illustrations" className="hover:text-gray-600">ILLUSTRATIONS</Link>
             </li>
@@ -45,17 +45,15 @@ const Header = () => {
                 download="Zolton_Resume"
                 to={resumePdf} 
                 target='_blank'
-              >
-                RESUME
+              >RESUME
               </Link>
             </li>
           </ul>
         </nav>
       </div>
-      <h4 className="text-left p-5 pl-7" style={{ fontFamily: 'FranklinGothic URW, sans-serif', color: '#6ac9cb', fontSize: '18px' }}>
-        Graphic Designer, Illustrator, and Web Designer based in Cleveland, OH.
-      </h4>
+ 
     </header>
+    
   );
 };
 
